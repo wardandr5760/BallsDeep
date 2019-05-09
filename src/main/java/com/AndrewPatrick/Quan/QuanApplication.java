111e89c21bc49558ee27c8147bb76973f5855637
wardandr576 <andrewward@cmail.carleton.ca>
2019-05-09 16:46:15 -0400

    Initial commit

---
+++ src/main/java/com/AndrewPatrick/Quan/QuanApplication.java
@@ -0,0 +1,13 @@
+package com.AndrewPatrick.Quan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanApplication.class, args);
+	}
+
+}
