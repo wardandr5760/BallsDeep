09f792e38b2f62302b5db3f3a425aa184f554c1b
wardandr576 <andrewward@cmail.carleton.ca>
2019-05-09 17:34:00 -0400

    controller change

--- src/main/java/com/AndrewPatrick/Quan/controllers/TestController.java
+++ src/main/java/com/AndrewPatrick/Quan/controllers/TestController.java
@@ -1,9 +1,9 @@
 package com.AndrewPatrick.Quan.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
     @RequestMapping("/")
